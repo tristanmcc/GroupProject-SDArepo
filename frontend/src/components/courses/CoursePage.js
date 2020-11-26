a4fb07c0dee819aa77349f80e8ff6b58c7a9798f
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-26 17:07:35 +0100

    added button link to course details page

--- frontend/src/components/courses/CoursePage.js
+++ frontend/src/components/courses/CoursePage.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function CoursePage() {
+        
     return (
         <div>
+            <Link to= {`/courseDetails`}>
+                <button  className=" btn btn-info btn-sm">
+                        click here to connect to course details page
+                </button>
+            </Link>
         </div>
     );
 }
