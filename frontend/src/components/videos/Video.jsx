a7e037ec709601919c7afe8536cb0b4d5ed9aa89
Lee Fowler <fowler.lee8@gmail.com>
2020-11-26 02:02:41 +0100

    made the videos page and added videos

---
+++ frontend/src/components/videos/Video.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Video({ data }) {
+  const {
+    id,
+    title,
+    description,
+    channelName,
+    channelThumb,
+    views,
+    videoThumb,
+  } = data;
+
+  return (
+    <article>
+      <Link to={`video/${id}`}>
+        <img src={videoThumb} alt={description}></img>
+      </Link>
+
+      {/* This could be a sub-component (atom)? */}
+      <aside>
+        <div>
+          <img src={channelThumb} alt="Channel thumbnail" />
+        </div>
+        <div>
+          <h3>{title}</h3>
+          <p>{channelName}</p>
+          <p>{views} views</p>
+        </div>
+      </aside>
+    </article>
+  );
+}
