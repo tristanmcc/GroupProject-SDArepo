81861983590a1ec898a98cf370a9223cb2d32d80
smitga <kota.smitha@gmail.com>
2020-11-26 17:00:00 +0100

    added course details page

---
+++ frontend/src/components/courses/Coursedetails.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Coursedetails() {
+    return (
+     <div>
+           <a className="nav-item">
+      <Link to="/courses" className="nav-link"> <button class="btn btn-primary btn-sm mt-4" type="submit" >BACK TO COURSE PAGE</button> </Link> </a>      
+    
+</div>  );}
+export default Coursedetails;
+
