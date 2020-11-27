47ff709bc58fbf8f77803a2e8b2d764d3bb760bc
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-27 15:15:47 +0100

    added changes to courses page

--- frontend/src/components/courses/CoursePage.js
+++ frontend/src/components/courses/CoursePage.js
@@ -1,34 +1,23 @@
 import React from "react";
 import CourseCard from "./CourseCard";
-import information from '../../information.json';
-import { Link } from "react-router-dom";
+import information from "../../information.json";
 
 function CoursePage() {
-    const CourseCards = information.map((item) => {
-        return <CourseCard key={item.id} data={item} />;
-    });
+  const CourseCards = information.map((item) => {
+    return <CourseCard key={item.id} data={item} />;
+  });
 
-    console.log("card details",CourseCards);
   return (
     <div className="course-container">
-     
-        <div className="row-buttons">
-          <button className="btn btn-info course-button"> CREATE COURSE</button>
-        
-          <button className="btn btn-info course-button"> DELETE COURSE</button>
-        
-          <button className="btn btn-info course-button"> UPDATE COURSE</button>
-        </div>
-     
-      
-      <div className="course-grid">
-          {CourseCards}
+      <div className="row-buttons">
+        <button className="btn btn-info course-button"> CREATE COURSE</button>
+
+        <button className="btn btn-info course-button"> DELETE COURSE</button>
+
+        <button className="btn btn-info course-button"> UPDATE COURSE</button>
       </div>
-      {/* <Link to={`/courseDetails`}>
-        <button className=" btn btn-info btn-sm course-details">
-          click here to connect to course details page
-        </button>
-      </Link> */}
+
+      <div className="course-grid">{CourseCards}</div>
     </div>
   );
 }
