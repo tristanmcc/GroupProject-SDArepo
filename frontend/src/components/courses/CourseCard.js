6994a991c36d6185a864280019d07c8890406526
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-27 15:15:17 +0100

    added card component

--- frontend/src/components/courses/CourseCard.js
+++ frontend/src/components/courses/CourseCard.js
@@ -3,12 +3,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import maths from '../../images/maths.png';
 
-// Note the curly braces around props, to allow to prepare to break down the object into individual variables
 export default function CourseCard({ data }) {
-  // Here is where we do the break down the data
   const {
     id,
-    category,
     title,
     description,
     thumbnail,
@@ -18,11 +15,11 @@ export default function CourseCard({ data }) {
     <div className="course-card">
       <Link to={`/courseDetails`}>
       
-        <img className="courseImage" src= {maths} alt={description}></img>
+        <img className="courseImage" src= {maths} alt={title}/>
       </Link>
       <div className="card-content">
           <h3 className="card-title">{title}</h3>
-          {/* <p className="description">{description}</p> */}
+          {/* <p className="description">{description}</p>  */}
          
       </div>
     </div> 
