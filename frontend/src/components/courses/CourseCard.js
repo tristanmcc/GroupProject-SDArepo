6aff40cf3a3ef03527520bea07b71709a172a48b
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-27 16:59:01 +0100

    added classes for styling

--- frontend/src/components/courses/CourseCard.js
+++ frontend/src/components/courses/CourseCard.js
@@ -1,31 +1,20 @@
 // React core
 import React from "react";
 import { Link } from "react-router-dom";
-import maths from '../../images/maths.png';
+import maths from "../../images/maths.png";
 
 export default function CourseCard({ data }) {
-  const {
-    id,
-    title,
-    description,
-    thumbnail,
-  } = data;
+  const { id, title, description, thumbnail } = data;
 
   return (
     <div className="course-card">
       <Link to={`/courseDetails`}>
-      
-        <img className="courseImage" src= {maths} alt={title}/>
+        <img className="courseImage" src={maths} alt={title} />
       </Link>
       <div className="card-content">
-          <h3 className="card-title">{title}</h3>
-          {/* <p className="description">{description}</p>  */}
-         
+        <h3 className="card-title">{title}</h3>
+        {/* <p className="description">{description}</p>  */}
       </div>
-    </div> 
-
-   
-
-    
+    </div>
   );
 }
