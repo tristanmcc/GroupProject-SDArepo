dcf873db0df150d24750fa1a22cca46002e06496
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-30 19:09:10 +0100

    added direct image to src tag of card

--- frontend/src/components/courses/CourseCard.js
+++ frontend/src/components/courses/CourseCard.js
@@ -4,12 +4,14 @@ import { Link } from "react-router-dom";
 import maths from "../../images/maths.png";
 
 export default function CourseCard({ data }) {
-  const { id, title, description, thumbnail } = data;
+  const { title, description, thumbnail } = data;
+  console.log("image",data.thumbnail);
 
   return (
     <div className="course-card">
       <Link to={`/courseDetails`}>
-        <img className="courseImage" src={maths} alt={title} />
+     
+        <img className="courseImage"  src={maths} alt={title} />
       </Link>
       <div className="card-content">
         <h3 className="card-title">{title}</h3>
