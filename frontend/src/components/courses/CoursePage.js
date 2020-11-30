148c4df72be878be08d06a09de6f68a64d3883c5
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-30 19:10:01 +0100

    added dialog to create course button

--- frontend/src/components/courses/CoursePage.js
+++ frontend/src/components/courses/CoursePage.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import CourseCard from "./CourseCard";
+import CourseForm from "./CourseForm";
 import information from "../../information.json";
 
 function CoursePage() {
+  const [openForm, setOpenForm] = useState(false);
   const CourseCards = information.map((item) => {
     return <CourseCard key={item.id} data={item} />;
   });
 
+  const handleClickOpen = () => {
+      setOpenForm(true);
+  }
+
+  const handleClose = () => {
+      setOpenForm(false);
+  }
+
+
   return (
     <div className="course-container">
       <div className="row-buttons">
-        <button className="btn btn-info course-button"> CREATE COURSE</button>
+          { 
+            openForm ? <CourseForm onCancel={handleClose}/>
+          
+        :
+        <>
+        <button className="btn btn-info course-button" onClick={handleClickOpen}> CREATE COURSE</button>
 
         <button className="btn btn-info course-button"> DELETE COURSE</button>
 
         <button className="btn btn-info course-button"> UPDATE COURSE</button>
-      </div>
+        </>
+        }
+        </div>
 
       <div className="course-grid">{CourseCards}</div>
     </div>
