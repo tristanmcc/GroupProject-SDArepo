c1f94e67685a692c764445daf05e4016cae194c3
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-30 19:16:50 +0100

    added Coursesapi.js file to Api folder

---
+++ frontend/src/api/CoursesApi.js
@@ -0,0 +1,26 @@
+import Api from "./Api";
+
+class CoursesApi {
+    
+    getAllCourses() {
+        return Api.get('/courses');
+    }
+
+    getCourseById(id) {
+        return Api.get('/courses?courseId='+id);
+    }
+
+    createCourse(course) {
+        return Api.post('/courses',course);
+    }
+
+    updateCourse(course) {
+        return Api.put('./courses',course);
+    }
+
+    deleteCourse(id) {
+        return Api.delete('/courses'+id);
+    }
+}
+
+export default new CoursesApi();
