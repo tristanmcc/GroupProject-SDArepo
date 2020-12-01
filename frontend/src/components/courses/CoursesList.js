fab251438d57977c5a2266da5fee970ba3bd6050
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-12-01 21:05:27 +0100

    added onCreateCourse to the courses list

--- frontend/src/components/courses/CoursesList.js
+++ frontend/src/components/courses/CoursesList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Course from './Course';
 
-function CoursesList({courses}) {
+function CoursesList({courses, onCourseDelete}) {
     return <div className="course-grid">
         {
           courses.map(course => (<Course 
                 key={course.id}
                 course={course}
+                onCourseDelete={onCourseDelete}
                 />)
          )  
         }
