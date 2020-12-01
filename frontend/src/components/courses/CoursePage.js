868126d389e093bfd07d7c0ac110a443593ad82f
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-12-01 11:20:55 +0100

    added get all method to get all the courses from backend

--- frontend/src/components/courses/CoursePage.js
+++ frontend/src/components/courses/CoursePage.js
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
-import CourseCard from "./CourseCard";
+import React, { useState, useEffect } from "react";
 import CourseForm from "./CourseForm";
-import information from "../../information.json";
+// import information from "../../information.json";
+import CoursesApi from "../../api/CoursesApi";
+import CoursesList from "./CoursesList";
 
 function CoursePage() {
+  const[courses, setCourses] = useState([]);
   const [openForm, setOpenForm] = useState(false);
-  const CourseCards = information.map((item) => {
-    return <CourseCard key={item.id} data={item} />;
-  });
+  /* const CourseCards = information.map((item) => {
+    return <Course key={item.id} course={item} />;
+  }); */
+
+  const getAll = () => {
+      CoursesApi.getAllCourses()
+        .then((res) => {
+            setCourses(res.data.sort((a,b) => b.id - a.id));
+        })
+  }
+
+  useEffect(() => {
+      console.log("get all courses",getAll());
+    getAll();   
+  }, []);
+
 
   const handleClickOpen = () => {
       setOpenForm(true);
@@ -21,7 +36,7 @@ function CoursePage() {
   return (
     <div className="course-container">
       <div className="row-buttons">
-          {console.log(openForm)};
+        
         { 
             openForm ? <CourseForm open={openForm} onClose={handleClose}/>
           
@@ -35,8 +50,13 @@ function CoursePage() {
         </>
         }
         </div>
-
-      <div className="course-grid">{CourseCards}</div>
+       
+        <CoursesList
+            courses={courses}
+            
+        />
+        
+     
     </div>
   );
 }
