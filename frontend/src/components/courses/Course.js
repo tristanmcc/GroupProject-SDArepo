971720c6cb10cb9bf03b9ea69b3112fe95122540
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-12-01 21:03:47 +0100

    added buttons delete and update

--- frontend/src/components/courses/Course.js
+++ frontend/src/components/courses/Course.js
@@ -1,10 +1,14 @@
 // React course page
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faPen } from '@fortawesome/free-solid-svg-icons'
 import { Link } from "react-router-dom";
 import courseImg from "../../images/courses-icon.png";
 
-export default function Course({ course }) {
+export default function Course({ course, onCourseDelete }) {
   const { title, description } = course;
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const [show, setShow] = useState(false);
 
   return (
     <div className="course-card">
@@ -14,6 +18,10 @@ export default function Course({ course }) {
       <div className="card-content">
         <h3 className="card-title">{title}</h3>
         <p className="description">{description}</p>
+      </div>
+      <div className="course-card-buttons">
+        <button className="deleteButton" onClick={() => onCourseDelete(course)}><FontAwesomeIcon icon={faTrash}/></button>
+        <button className="updateButton"><FontAwesomeIcon icon={faPen}/></button>
       </div>
     </div>
   );
