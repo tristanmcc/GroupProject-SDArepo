011c2a8550f6b103dc8315d8d8cd3751ce4af641
Lee Fowler <fowler.lee8@gmail.com>
2020-12-01 16:44:04 +0100

    created the FE video upload features

--- frontend/src/components/videos/VideosPage.jsx
+++ frontend/src/components/videos/VideosPage.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 
+import axios from 'axios';
+
 import Video from './Video';
+import VideoUploading from './VideoUploading.jsx';
 
 import information from '../../resources/information.json';
 
 export default function VideosPage() {
+  const Uploader = () => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'presentUserToken',
+      },
+    };
+
+    axios
+      .post(
+        'http:localhost/8080/videos',
+        {
+          title: 'New Todo',
+          completed: false,
+        },
+        config
+      )
+      .then((res) => console.log(res.data))
+      .catch((err) => console.err(err));
+  };
+
   return (
     <>
       <h1>Lectures</h1>
       <Video data={information[6]} />
+
+      <h1>Upload feature</h1>
+      <VideoUploading clickUpload={Uploader} />
     </>
   );
 }
