cd9b81645837510a7778eeab97d23a2583a8e5e6
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-12-03 17:18:56 +0100

    updated the mapping between courses and videos

--- src/main/java/se/kth/sda/skeleton/video/VideoRepository.java
+++ src/main/java/se/kth/sda/skeleton/video/VideoRepository.java
@@ -3,8 +3,11 @@ package se.kth.sda.skeleton.video;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface VideoRepository extends JpaRepository <Video, Long> {
+    List<Video> findAllByCourseId(Long courseId);
 
 }
