a468fac9d583f652c87ec9edcfdda56ec06b8c39
Lee Fowler <fowler.lee8@gmail.com>
2020-12-08 13:53:19 +0100

    updated the design of the videos page with material UI and changed form submission

--- frontend/src/components/videos/VideoCard.jsx
+++ frontend/src/components/videos/VideoCard.jsx
@@ -1,33 +1,71 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-import coursesIcon from '../../images/courses-icon.png';
+import '../../CSS/videos.css';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faVideo } from '@fortawesome/free-solid-svg-icons';
+import DuoTwoToneIcon from '@material-ui/icons/DuoTwoTone';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 250,
+    backgroundColor: '#6AD1FA',
+    textAlign: 'center',
+  },
+  media: {
+    height: 20,
+  },
+});
 
 export default function VideoCard({ video, onDeleteClick, user }) {
   console.log('videocard print out: ' + video.videoUrl);
 
+  const classes = useStyles();
   return (
-    <div className="card mt-5 p-2">
-      <div className="comment">
-        <div className="signature">
+    <Card className={classes.root}>
+      <CardActionArea>
+        <IconButton>
           <a href={video.videoUrl} target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faVideo} size="6x" color="black" />
+            <DuoTwoToneIcon
+              style={{
+                height: 130,
+                width: '100%',
+                color: '#E55A71',
+                margin: '0',
+              }}
+            />
           </a>
+        </IconButton>
 
-          <span className="user-name-comment">{`video  name : ${video.videoName}`}</span>
-        </div>
-        <button
-          className="btn btn-danger"
-          onClick={() => {
-            onDeleteClick(video);
-          }}
-        >
-          <FontAwesomeIcon icon={faTrash} />
-        </button>
-      </div>
-    </div>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {`${video.videoName}`}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+
+      <button
+        style={{
+          height: 60,
+          color: 'black',
+          backgroundColor: '#E55A71',
+          width: '100%',
+        }}
+        onClick={() => {
+          onDeleteClick(video);
+        }}
+      >
+        <DeleteOutlineOutlinedIcon style={{ height: 60, width: '100%' }} />
+      </button>
+    </Card>
   );
 }
--- frontend/src/components/videos/Videos.jsx
+++ frontend/src/components/videos/Videos.jsx
@@ -4,8 +4,13 @@ import Api from '../../api/Api';
 import VideoCard from './VideoCard';
 import VideoUploadForm from './VideoUploadForm';
 
+import Grid from '@material-ui/core/Grid';
+
+import '../../CSS/videos.css';
+
 export default function Videos({ user }) {
   const [videos, setVideos] = useState([]);
+  console.log('from videos line: ' + videos);
 
   const createVideo = (videoData) => {
     console.log('videodata : ' + videoData);
@@ -34,19 +39,25 @@ export default function Videos({ user }) {
     getVideos();
   }, []);
 
+  const Cards = videos.map((item) => {
+    return (
+      <div className="videoLayout">
+        <VideoCard
+          key={item.id}
+          video={item}
+          onDeleteClick={deleteVideo}
+          user={user}
+        />
+      </div>
+    );
+  });
+
   return (
     <div>
       <div>
         <VideoUploadForm onCreateClick={createVideo} user={user} />
 
-        {videos.map((video) => (
-          <VideoCard
-            key={video.id}
-            video={video}
-            onDeleteClick={deleteVideo}
-            user={user}
-          />
-        ))}
+        <div>{Cards}</div>
       </div>
     </div>
   );
