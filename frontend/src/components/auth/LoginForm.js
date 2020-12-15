627ab9e9fb8852419cc064f7a3c7d3986eb91c70
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-12-15 09:42:03 +0100

    added required to form input fields

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -1,100 +1,127 @@
-import React, {useState} from 'react';
-import '../../css/styles.css';
-import book from '../../images/undraw_education_f8ru.svg';
-import history from '../../images/undraw_voice_interface_eckp.svg';
+import React, { useState } from "react";
+import "../../css/styles.css";
+import book from "../../images/undraw_education_f8ru.svg";
+import history from "../../images/undraw_voice_interface_eckp.svg";
 
-function LoginForm({onSubmit,onRegister}) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [name, setName] = useState("");
-    const [userRole, setUserRole] = useState("");
-   
-   function changeModeAdd()
-   {
-        const container = document.querySelector(".login-container");
-        container.classList.add("sign-up-mode")
+function LoginForm({ onSubmit, onRegister }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [userRole, setUserRole] = useState("");
 
-   }
-   function changeModeRemove()
-   {
-        const container1 = document.querySelector(".login-container");
-        container1.classList.remove("sign-up-mode");
-   }
+  function changeModeAdd() {
+    const container = document.querySelector(".login-container");
+    container.classList.add("sign-up-mode");
+  }
+  function changeModeRemove() {
+    const container1 = document.querySelector(".login-container");
+    container1.classList.remove("sign-up-mode");
+  }
 
-    
-    return (
-
-      <div class="login-container">
-             <div class="forms-container">
-             <div class="signin-signup">
+  return (
+    <div class="login-container">
+      <div class="forms-container">
+        <div class="signin-signup">
           <form class="sign-in-form">
             <h2 class="login-title">Sign in</h2>
             <div class="input-field">
               <i class="fas fa-user"></i>
-              <input type="email" 
-              placeholder="Email" 
-              value={email} 
-              onChange={e => setEmail(e.target.value)} />
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div class="input-field">
               <i class="fas fa-lock"></i>
-              <input 
-              type="password" 
-              placeholder="Password"
-              value={password}
-              onChange={e => setPassword(e.target.value)} />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
-            <input type="button" value="Login" onClick={() => onSubmit({email, password})} class="login-btn solid" />
-            
+            <input
+              type="button"
+              value="Login"
+              onClick={() => onSubmit({ email, password })}
+              class="login-btn solid"
+            />
           </form>
           <form class="sign-up-form">
             <h2 class="login-title">Sign up</h2>
             <div class="input-field">
               <i class="fas fa-user"></i>
-              <input type="text" placeholder="Name" value={name} onChange={ e => setName(e.target.value) }/>
+              <input
+                type="text"
+                placeholder="Name"
+                value={name}
+                required
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div class="input-field">
               <i class="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" value={email} onChange={ e => setEmail(e.target.value) }/>
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div class="input-field">
               <i class="fas fa-lock"></i>
-              <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
 
             <div class="social-media">
-           
-            <input 
-            className="social-icon" 
-            type="radio"  
-            name="userRole" 
-            value={"teacher"} 
-            onChange={e => setUserRole(e.target.value)}/> 
-            Teacher
-            <input className="social-icon" 
-            type="radio"  
-            name="userRole" 
-            value={"student"} 
-            onChange={e => setUserRole(e.target.value)}/> 
-            Student
+              <input
+                className="social-icon"
+                type="radio"
+                name="userRole"
+                value={"teacher"}
+                onChange={(e) => setUserRole(e.target.value)}
+              />
+              Teacher
+              <input
+                className="social-icon"
+                type="radio"
+                name="userRole"
+                value={"student"}
+                onChange={(e) => setUserRole(e.target.value)}
+              />
+              Student
             </div>
-            
-            <input type="button" class="login-btn" value="Sign up" onClick={e => onRegister({name, email, password, userRole})}/>
-            
-           
-            
+
+            <input
+              type="button"
+              class="login-btn"
+              value="Sign up"
+              onClick={(e) => onRegister({ name, email, password, userRole })}
+            />
           </form>
-          </div>
-        
+        </div>
       </div>
       <div class="panels-container">
         <div class="login-panel left-panel">
           <div class="login-content">
             <h3>Ready to join?</h3>
             <p>
-              Take  the  first  step  of  your  journey  to  knowledge  with  us. Join now
+              Take the first step of your journey to knowledge with us. Join now
             </p>
-            <button class="login-btn transparent" id="sign-up-btn" onClick={() => changeModeAdd()} > 
+            <button
+              class="login-btn transparent"
+              id="sign-up-btn"
+              onClick={() => changeModeAdd()}
+            >
               Sign up
             </button>
           </div>
@@ -102,11 +129,13 @@ function LoginForm({onSubmit,onRegister}) {
         </div>
         <div class="login-panel right-panel">
           <div class="login-content">
-            <h3>Already registered ?</h3>    
-            <p>
-                Explore the limitless world of Learning
-            </p>
-            <button class="login-btn transparent" id="sign-in-btn" onClick={() => changeModeRemove()}>
+            <h3>Already registered ?</h3>
+            <p>Explore the limitless world of Learning</p>
+            <button
+              class="login-btn transparent"
+              id="sign-in-btn"
+              onClick={() => changeModeRemove()}
+            >
               Sign in
             </button>
           </div>
@@ -114,10 +143,7 @@ function LoginForm({onSubmit,onRegister}) {
         </div>
       </div>
     </div>
-               
-
-                    
-    );
+  );
 }
 
 export default LoginForm;
