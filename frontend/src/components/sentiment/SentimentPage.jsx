6563714d0cea77b9022a4ba05ad48319ea3ded0d
Lee Fowler <fowler.lee8@gmail.com>
2020-12-17 16:38:26 +0100

    updated the sentiment page

--- frontend/src/components/sentiment/SentimentPage.jsx
+++ frontend/src/components/sentiment/SentimentPage.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-import Sentiment from "./Sentiment";
+import React from 'react';
+import Sentiment from './Sentiment';
 
 export default function SentimentPage() {
   return (
     <>
-      <div className="student-banner">Question Sentiment</div>
-      <hr />
-      <div style={{ textAlign: "center" }}>
-        <Sentiment />
+      <div className="card">
+        <div className="student-banner">Question Sentiment</div>
+
+        <div style={{ textAlign: 'center', marginTop: '25px' }}>
+          <Sentiment />
+        </div>
       </div>
     </>
   );
