d78adbe23012ad07e3f1dfdeb2e9137a766d3634
Lee Fowler <fowler.lee8@gmail.com>
2020-12-18 00:40:32 +0100

    updated gitignore

--- frontend/src/api/Api.js
+++ frontend/src/api/Api.js
@@ -1,32 +1,31 @@
-import axios from "axios";
-import Auth from "../services/Auth";
+import axios from 'axios';
+import Auth from '../services/Auth';
 
-const BASE_URL = "https://git.heroku.com/aqueous-temple-16710.git";
+const BASE_URL = 'https://aqueous-temple-16710.herokuapp.com';
 
 const Api = axios.create({
-    baseURL: BASE_URL,
+  baseURL: BASE_URL,
 });
 
 Api.interceptors.request.use((config) => {
-    if (Auth.isLoggedIn()) {
-        const authHeader = Auth.getAuthorizationHeader();
-        config.headers['common']['authorization'] = authHeader;
-    }
-    
-    return config;
+  if (Auth.isLoggedIn()) {
+    const authHeader = Auth.getAuthorizationHeader();
+    config.headers['common']['authorization'] = authHeader;
+  }
+
+  return config;
 });
 
 Api.interceptors.response.use(
-    r => r,
-    (err) => {
-        if (err.response && [401, 403].indexOf(err.response.status) !== -1) {
-            Auth.logout();
-        }
-
-        return Promise.reject(err);
+  (r) => r,
+  (err) => {
+    if (err.response && [401, 403].indexOf(err.response.status) !== -1) {
+      Auth.logout();
     }
-)
 
+    return Promise.reject(err);
+  }
+);
 
 // Exporting Api into the global namespace for introspecting
 window.Api = Api;
